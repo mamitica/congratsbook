b6ee7412bdf8dbd4a727d238a18eef5754eccf5e
alearends <alejandroarends77@gmail.com>
2024-07-07 17:37:01 +0200

    solucionado envio de email y limpieza de formulario

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
-'use strict';
-
-
+"use strict";
 
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function (elem) {
+    elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
+sidebarBtn.addEventListener("click", function () {
+    elementToggleFunc(sidebar);
+});
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
@@ -31,29 +29,27 @@ const modalText = document.querySelector("[data-modal-text]");
 const testimonialsModalFunc = function () {
     modalContainer.classList.toggle("active");
     overlay.classList.toggle("active");
-}
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
     testimonialsItem[i].addEventListener("click", function () {
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector(
+        "[data-testimonials-title]"
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+        "[data-testimonials-text]"
+    ).innerHTML;
 
-        modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-        modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-        modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-        modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-        testimonialsModalFunc();
-
+    testimonialsModalFunc();
     });
-
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
@@ -61,17 +57,17 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select.addEventListener("click", function () {
+    elementToggleFunc(this);
+});
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
     selectItems[i].addEventListener("click", function () {
-
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        elementToggleFunc(select);
-        filterFunc(selectedValue);
-
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
     });
 }
 
@@ -79,41 +75,31 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
     for (let i = 0; i < filterItems.length; i++) {
-
-        if (selectedValue === "all") {
-            filterItems[i].classList.add("active");
-        } else if (selectedValue === filterItems[i].dataset.category) {
-            filterItems[i].classList.add("active");
-        } else {
-            filterItems[i].classList.remove("active");
-        }
-
+    if (selectedValue === "all") {
+        filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+        filterItems[i].classList.add("active");
+    } else {
+        filterItems[i].classList.remove("active");
     }
-
-}
+    }
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-
     filterBtn[i].addEventListener("click", function () {
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
 
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        filterFunc(selectedValue);
-
-        lastClickedBtn.classList.remove("active");
-        this.classList.add("active");
-        lastClickedBtn = this;
-
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
     });
-
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
@@ -123,40 +109,46 @@ const formBtn = document.querySelector("[data-form-btn]");
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
     formInputs[i].addEventListener("input", function () {
-
-        // check form validation
-        if (form.checkValidity()) {
-            formBtn.removeAttribute("disabled");
-        } else {
-            formBtn.setAttribute("disabled", "");
-        }
-
+    // check form validation
+    if (form.checkValidity()) {
+        formBtn.removeAttribute("disabled");
+    } else {
+        formBtn.setAttribute("disabled", "");
+    }
     });
 }
 
 //send email by JS
-const btn = document.getElementById('buttonJS');
+const btn = document.getElementById("buttonJS");
 
-document.getElementById('formJS')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+document.getElementById("formJS").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.textContent = "Sending...";
 
-   const serviceID = 'default_service';
-   const templateID = 'template_ytie91w';
+    const serviceID = "default_service";
+    const templateID = "template_ytie91w";
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('ya recibimos tu mensaje! ツ ');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+  // Debug: Log form data before sending
+    const formData = new FormData(this);
+    console.log("Form data before sending:");
+    formData.forEach((value, key) => {
+    console.log(key, value);
     });
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+        btn.textContent = "Send Message";
+        alert("✅ ya recibimos tu mensaje! ツ ");
+      form.reset(); // Limpia formulario después de enviar el mensaje
+      formBtn.setAttribute("disabled", ""); // Deshabilita botón enviar hasta que se llene formulario otra vez
+    },
+    (err) => {
+        btn.textContent = "Send Message";
+        alert(JSON.stringify(err));
+    }
+    );
 });
-
-
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -165,17 +157,15 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].addEventListener("click", function () {
-
-        for (let i = 0; i < pages.length; i++) {
-            if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-                pages[i].classList.add("active");
-                navigationLinks[i].classList.add("active");
-                window.scrollTo(0, 0);
-            } else {
-                pages[i].classList.remove("active");
-                navigationLinks[i].classList.remove("active");
-            }
+    for (let i = 0; i < pages.length; i++) {
+        if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+        } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
         }
-
+    }
     });
 }
